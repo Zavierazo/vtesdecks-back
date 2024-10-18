cbf8b3ff88729488ae7dda2e75aa030a634dcaa4
Zavierazo <zavierazo@gmail.com>
2024-10-18 13:59:44 +0200

    Add percentage to statisctics

--- src/main/java/com/vtesdecks/model/api/ApiStatistic.java
+++ src/main/java/com/vtesdecks/model/api/ApiStatistic.java
@@ -3,9 +3,12 @@ package com.vtesdecks.model.api;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiStatistic {
     private String label;
     private Integer count = 0;
+    private BigDecimal percentage = BigDecimal.ZERO;
 }
