fb30b6e0eb9f1485410f26224da5b9d63f3f5995
Zavierazo <zavierazo@gmail.com>
2025-12-05 20:43:00 +0100

    feat(ApiErrorController): enhance error logging with user-agent and IP address details

--- src/main/java/com/vtesdecks/api/controller/ApiErrorController.java
+++ src/main/java/com/vtesdecks/api/controller/ApiErrorController.java
@@ -1,5 +1,8 @@
 package com.vtesdecks.api.controller;
 
+import com.vtesdecks.util.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,19 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Controller
 @RequestMapping("/api/1.0/error")
 @Slf4j
 public class ApiErrorController {
 
     @RequestMapping(method = RequestMethod.POST, produces = {
-        MediaType.APPLICATION_JSON_VALUE
+            MediaType.APPLICATION_JSON_VALUE
     })
     @ResponseBody
-    public ResponseEntity<Boolean> set(@RequestBody String message) {
-        log.warn("Client side error: {}", message);
+    public ResponseEntity<Boolean> set(HttpServletRequest httpServletRequest, @RequestBody String message) {
+        log.warn("Client side error User-Agent {} Ip {}: {} ", httpServletRequest.getHeader("User-Agent"), Utils.getIp(httpServletRequest), message);
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 }
