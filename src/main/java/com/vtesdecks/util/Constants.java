4ec269c205f63aeeaa00d8bb6e80cab362a47cf5
Zavierazo <zavierazo@gmail.com>
2025-12-28 13:40:04 +0100

    feat(WebConfiguration): expose DATE_HEADER in CORS configuration

--- src/main/java/com/vtesdecks/util/Constants.java
+++ src/main/java/com/vtesdecks/util/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
     public static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm");
     public static final String CARDS_DELETED_HEADER = "X-Card-Deleted";
     public static final String CONTENT_DISPOSITION_HEADER = "Content-Disposition";
+    public static final String DATE_HEADER = "Date";
 
 }
