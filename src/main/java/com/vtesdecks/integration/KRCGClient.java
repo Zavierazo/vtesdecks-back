bf592c586e83c3a1b86c26c2c14937b8319d9cda
Zavierazo <zavierazo@gmail.com>
2026-01-23 19:58:13 +0100

    fix(krcg): ignore ssl for all krcg api calls

--- src/main/java/com/vtesdecks/integration/KRCGClient.java
+++ src/main/java/com/vtesdecks/integration/KRCGClient.java
@@ -2,9 +2,12 @@ package com.vtesdecks.integration;
 
 import com.vtesdecks.model.krcg.Card;
 import com.vtesdecks.model.krcg.Deck;
+import feign.Client;
 import feign.Logger;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,6 +18,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
@@ -33,6 +41,7 @@ public interface KRCGClient {
     @GetMapping(value = "/card/{id}", produces = APPLICATION_JSON_VALUE)
     Card getCard(@PathVariable("id") Integer id);
 
+    @Slf4j
     class Configuration {
         @Bean
         public Logger.Level feignLoggerLevel() {
@@ -42,6 +51,39 @@ public interface KRCGClient {
         @Bean
         public Encoder feignFormEncoder(ObjectFactory<HttpMessageConverters> converters) {
             return new SpringFormEncoder(new SpringEncoder(converters));
+        }
+
+        @Bean
+        public Client feignClient() {
+            try {
+                // Trust all certificates
+                TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            public X509Certificate[] getAcceptedIssuers() {
+                                return new X509Certificate[0];
+                            }
+
+                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                            }
+
+                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                            }
+                        }
+                };
+
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustAllCerts, new SecureRandom());
+
+                OkHttpClient ok = new OkHttpClient.Builder()
+                        .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                        .hostnameVerifier((hostname, session) -> true)
+                        .build();
+
+                return new feign.okhttp.OkHttpClient(ok);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                return null;
+            }
         }
     }
 }
