aacc8e8717c4f9e3324cccda5a3a1903072959d8
Zavierazo <zavierazo@gmail.com>
2026-07-05 13:04:23 +0200

    feat(ApiRuling): add mapping for RulingSymbol to ApiRulingSymbol

--- src/main/java/com/vtesdecks/api/mapper/ApiCardInfoMapper.java
+++ src/main/java/com/vtesdecks/api/mapper/ApiCardInfoMapper.java
@@ -1,7 +1,10 @@
 package com.vtesdecks.api.mapper;
 
 import com.vtesdecks.model.api.ApiRuling;
+import com.vtesdecks.model.api.ApiRulingSymbol;
 import com.vtesdecks.model.krcg.Ruling;
+import com.vtesdecks.model.krcg.RulingSymbol;
+import com.vtesdecks.util.VtesUtils;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -11,5 +14,24 @@ public abstract class ApiCardInfoMapper {
 
     public abstract List<ApiRuling> mapRulings(List<Ruling> rulings);
 
+
+    protected ApiRulingSymbol mapRulingSymbol(RulingSymbol rulingSymbol) {
+        if (rulingSymbol == null || rulingSymbol.getText() == null || rulingSymbol.getText().length() < 2) {
+            return null;
+        }
+        String disciplineAlias = rulingSymbol.getText().substring(1, rulingSymbol.getText().length() - 1);
+
+        String symbol = VtesUtils.getDisciplineIconFromAbbreviation(disciplineAlias);
+
+        if (symbol == null) {
+            return null;
+        }
+
+        return ApiRulingSymbol
+                .builder()
+                .text(disciplineAlias)
+                .symbol(symbol)
+                .build();
+    }
 
 }
